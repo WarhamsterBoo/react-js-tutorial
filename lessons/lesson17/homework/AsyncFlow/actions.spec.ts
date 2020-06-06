ba16a974b9935806d71cbf0307f873e1c0e32fce
WarhamsterBoo <warhamsterboo@gmail.com>
2020-06-06 21:09:47 +0300

    add success and failure action creators

--- lessons/lesson17/homework/AsyncFlow/actions.spec.ts
+++ lessons/lesson17/homework/AsyncFlow/actions.spec.ts
@@ -1,10 +1,28 @@
-import { loading } from "./actions";
+import { failure, loading, success } from "./actions";
 
 describe("action creators", () => {
   it("should create action to set loading", () => {
     expect(loading()).toEqual({
       type: "LOADING",
       payload: undefined,
+    });
+  });
+
+  it("should create action to note fetching success", () => {
+    const payload = { data: 1, error: undefined };
+
+    expect(success(payload)).toEqual({
+      type: "SUCCESS",
+      payload,
+    });
+  });
+
+  it("should create action to note fetching error", () => {
+    const payload = { data: undefined, error: "something went wrong" };
+
+    expect(failure(payload)).toEqual({
+      type: "FAILURE",
+      payload,
     });
   });
 });
--- lessons/lesson17/homework/AsyncFlow/actions.ts
+++ lessons/lesson17/homework/AsyncFlow/actions.ts
@@ -1,3 +1,10 @@
 import { createAction } from "@reduxjs/toolkit";
 
+type Payload = {
+  data: unknown | undefined;
+  error?: unknown | undefined;
+};
+
 export const loading = createAction("LOADING");
+export const success = createAction<Payload>("SUCCESS");
+export const failure = createAction<Payload>("FAILURE");

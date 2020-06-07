7a1f01d42191fbbe6866c0ee29893c899ab05aae
WarhamsterBoo <warhamsterboo@gmail.com>
2020-06-07 19:18:35 +0300

    trying to fix ci

--- lessons/lesson7/Images.stories.tsx
+++ lessons/lesson7/Images.stories.tsx
@@ -5,18 +5,18 @@ import { ImageLocal } from "./ImageLocal";
 import { ImageRandom } from "./ImageRandom";
 import { ImageServer } from "./ImageServer";
 
-storiesOf("Images", module)
-  .addDecorator(withKnobs)
-  .lokiSkip("RandomImages", () => (
-    <ImageRandom
-      key="jsx"
-      interval={number("interval", 1000)}
-      isActive={boolean("is active", true)}
-    />
-  ))
-  .lokiSkip("LocallyCreatedURL", () => (
-    <ImageLocal key="jsx" id={number("id", 3)} />
-  ))
-  .lokiSkip("RemotelyCreatedURL", () => (
-    <ImageServer key="jsx" id={number("id", 7)} />
-  ));
+// storiesOf("Images", module)
+//   .addDecorator(withKnobs)
+//   .lokiSkip("RandomImages", () => (
+//     <ImageRandom
+//       key="jsx"
+//       interval={number("interval", 1000)}
+//       isActive={boolean("is active", true)}
+//     />
+//   ))
+//   .lokiSkip("LocallyCreatedURL", () => (
+//     <ImageLocal key="jsx" id={number("id", 3)} />
+//   ))
+//   .lokiSkip("RemotelyCreatedURL", () => (
+//     <ImageServer key="jsx" id={number("id", 7)} />
+//   ));

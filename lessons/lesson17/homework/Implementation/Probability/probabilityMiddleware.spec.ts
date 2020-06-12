385a5a9b3fa2d45a63ae12632fbab4f23d6064e3
WarhamsterBoo <warhamsterboo@gmail.com>
2020-06-12 20:53:15 +0300

    add test: probabilityMiddleware should consider probability when executing action

--- lessons/lesson17/homework/Implementation/Probability/probabilityMiddleware.spec.ts
+++ lessons/lesson17/homework/Implementation/Probability/probabilityMiddleware.spec.ts
@@ -1,6 +1,15 @@
 import { probabilityMiddleware } from "./probabilityMiddleware";
 
 describe("probabilityMiddleware", () => {
+  const create = () => {
+    return {
+      action: jest.fn(),
+      next: jest.fn(),
+      dispatch: jest.fn(),
+      getState: jest.fn(),
+    };
+  };
+
   it.each`
     action
     ${{ type: "SOME_ACTION" }}
@@ -8,10 +17,10 @@ describe("probabilityMiddleware", () => {
   `(
     "should execute action if it has no probability in payload",
     ({ action }) => {
-      const next = jest.fn();
+      const { next, dispatch, getState } = create();
       const sut = probabilityMiddleware({
-        dispatch: jest.fn(),
-        getState: jest.fn(),
+        dispatch,
+        getState,
       });
 
       sut(next)(action);
@@ -22,10 +31,10 @@ describe("probabilityMiddleware", () => {
   );
 
   it("should not execute aciton if probability is equal to 0", () => {
-    const next = jest.fn();
+    const { next, dispatch, getState } = create();
     const sut = probabilityMiddleware({
-      dispatch: jest.fn(),
-      getState: jest.fn(),
+      dispatch,
+      getState,
     });
     const action = { type: "SOME_ACTION", payload: { probability: 0 } };
 
@@ -35,10 +44,10 @@ describe("probabilityMiddleware", () => {
   });
 
   it("should execute aciton if probability is equal to 1", () => {
-    const next = jest.fn();
+    const { next, dispatch, getState } = create();
     const sut = probabilityMiddleware({
-      dispatch: jest.fn(),
-      getState: jest.fn(),
+      dispatch,
+      getState,
     });
     const action = { type: "SOME_ACTION", payload: { probability: 1 } };
 
@@ -46,5 +55,20 @@ describe("probabilityMiddleware", () => {
 
     expect(next).toBeCalledTimes(1);
     expect(next).toBeCalledWith(action);
+  });
+
+  it("should consider probability when executing action", () => {
+    const { next, dispatch, getState } = create();
+    const sut = probabilityMiddleware({
+      dispatch,
+      getState,
+    });
+    const action = { type: "SOME_ACTION", payload: { probability: 0.5 } };
+    Math.random = jest.fn().mockReturnValueOnce(0.1).mockReturnValueOnce(0.9);
+
+    sut(next)(action);
+    sut(next)(action);
+
+    expect(next).toBeCalledTimes(1);
   });
 });
